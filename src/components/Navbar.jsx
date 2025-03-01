7e28f5ad9052c33c0e6410dd8265651fa1ae3c0d
Nikitha-gr <nikitha14.gr@gmail.com>
2025-03-01 17:34:29 +0530

    Books Library Management App

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
+import { Link as RouterLink} from "react-router-dom";
 
 const Navbar = () => {
     const [books] = useSelector((state)=>state.books);
 
     return (
         <Box bg="blue.500" p={4} color="white">
-            
+            <Flex justifyContent={"space-evenly"} align="center">
+                <Heading>My Library</Heading>
+                <Flex gap={4} align="center">
+                    <Link as={RouterLink} to="/">Home</Link>
+                    <Link as={RouterLink} to="/login">Login</Link>
+                    <Link as={RouterLink} to="/register">Register</Link>
+
+                </Flex>
+            </Flex>
         </Box>
     )
 }
